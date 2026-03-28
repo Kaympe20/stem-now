047a940d46ab01b1aaaefcf48de95d8ffec76d68
Kayla Blosser <kayla.blosser@proton.me>
2026-03-27 22:29:37 -0400

    Fix title

--- app/app.vue
+++ app/app.vue
@@ -11,11 +11,11 @@ onMounted(async () => {
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1>Stem Now</h1>
+    <h1 class="text-5xl w-full text-center mt-3 font-black underline">Makerspaces</h1>
     <div v-for="location in agregatedJSON" :key="location.name">
       <div class="flex items-center justify-center gap-3">
         <div class="w-[60vh] min-h-50 flex flex-col justify-center">
-          <h2 class="text-2xl font-bold underline mb-2">{{ location.name }}</h2>
+          <h2 class="text-2xl font-bold mb-2">{{ location.name }}</h2>
           <p>{{ location.description }}</p>
         </div>
         <img :src="location.imageUrl.toString()" alt="Location Image" class="w-32 h-32 object-cover rounded-md" />
